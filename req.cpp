8869fae18e33c0e21649252a957ed9b07cf105ee
Lord <lucasmendes.lx@gmail.com>
2020-03-24 12:03:01 -0300

    Comented the .cpp file

--- req.cpp
+++ req.cpp
@@ -1,10 +1,23 @@
 #include "req.h"
+/*!
+* Lib to perform Json's APIs consumption (GET, PUT and POST)
+*
+* @Author Lucas C. Mendes
+*/
 
+/*!
+* Constructor gets the adress of the API
+* @param QString
+*/
 req::req(QString adress)
 {
     this.adress = adress;
 }
-
+/*!
+* perform a POST request to the API server in the desired route
+*@param QJsonObject , the posted object
+*@param QString , the route
+*/
 void req::post(QJsonObject object, QString rota){
     QNetworkAccessManager man;
     QJsonObject json;
@@ -23,7 +36,11 @@ void req::post(QJsonObject object, QString rota){
         qDebug()<< "Error : "<<reply->errorString();
     }
 }
-
+/*!
+* Returns a QJsonDocument with the JSON of the inserted route
+* @param QString, the route
+* @return QJsonDocument, the Json document of the inserted route
+*/
 QJsonDocument req::get(QString rota){
     QNetworkAccessManager man;
     QJsonObject json;
@@ -49,7 +66,11 @@ QJsonDocument req::get(QString rota){
 
     return jsonDoc;
 }
-
+/*!
+* Perform a PUT request in the JSON of the desired route
+* @param QJsonObject, the new object with the alterations
+* @param Qstring, the route
+*/
 void req::put(QJsonObject object, QString rota){
     QNetworkAccessManager man;
     QJsonObject json;
